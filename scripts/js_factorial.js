b54de9776dcf1b6cd4d9b6f20987c486d9e946d5
sushish-kumar <sushish.kumar.cse14@iitbhu.ac.in>
2018-10-16 23:48:01 +0530

    modify js_factorial.js
    there was a common mistake assuming factorial as sequence.

--- scripts/js_factorial.js
+++ scripts/js_factorial.js
@@ -1,34 +1,34 @@
 // return the sum of a consecutive series of nums 1...n
 
 function factorial(n) {
-  let sum=0;
+  let fact = 1;
   for(let i=1; i<=n; i++){
-    sum+=i;
+    fact*=i;
   }
-  return sum;
+  return fact;
 }
 
 function factorial2(n) {
   if(n===1){
     return 1;
   }
-  return n + factorial(n-1);
+  return n * factorial(n-1);
 }
 
 // factorial(5)
-//   5 + factorial(4)
-//     5 + 4 + factorial(3)
-//       5 + 4 + 3 + factorial(2)
-//         5 + 4 + 3 + 2 + factorial(1)
-//           5 + 4 + 3 + 2 + 1
+//   5 * factorial(4)
+//     5 * 4 * factorial(3)
+//       5 * 4 * 3 * factorial(2)
+//         5 * 4 * 3 * 2 * factorial(1)
+//           5 * 4 * 3 * 2 * 1
 
 function factorial3(n){
-  let sum=0;
+  let fact = 1;
   while(n>=1){
-    sum+=n;
+    fact*=n;
     n--;
   }
-  return sum;
+  return fact;
 }
 
 console.log(
